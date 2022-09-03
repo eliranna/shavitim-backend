10964953eeeb56de6d9c8cab04ba7c165a68b3fe
eliran-natan <eliran.natan@axonius.com>
2022-09-04 02:45:00 +0300

    sds

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
 module.exports = router;
